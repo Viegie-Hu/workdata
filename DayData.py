515bf0e59352158c9ddf1e2c3b3074535760f7f6
jie <18508486196@163.com>
2023-06-09 14:05:03 +0800

    modify 6.9 14：04

--- DayData.py
+++ DayData.py
@@ -22,7 +22,7 @@ class Daydata():
 	    df_cy['时间'] = pd.to_datetime(df['开始答题时间']).dt.date
 	    df_cy['时间'] = pd.to_datetime(df_cy['时间'])
 	    df_cy['时间2'] = daytime.today()
-	    df_cy[df_cy['时间']==df_cy['时间2']].iloc[:,:-1].sort_values('销售额',ascending=False)
+	    df_today = df_cy[df_cy['时间']==df_cy['时间2']].iloc[:,:-1].sort_values('销售额',ascending=False)
 	    df_all = pd.pivot_table(df_cy,index = ['门店','销售'],values=['接待','电销','留资（电话）',
 	        '加微','售卡','派函','开单','销售额'],
 	        aggfunc='sum').sort_values('销售额',ascending=False).reset_index()
